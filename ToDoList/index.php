22576754636e1ff1b774d519b351941308b406e9
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-04-10 15:03:34 +0800

    Update index.php

--- ToDoList/index.php
+++ ToDoList/index.php
@@ -1,1 +1,27 @@
-
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>To-Do</title>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+		<link rel="stylesheet" href="C:\Users\Roshni\Documents\Y12 Computer Science\php\to\main.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+		<div class = :list">
+			<h1 class="header">To do </h1>
+			<ul class="items">
+				<li>
+					<span class = "item">Pick up shopping</span>
+					<a href="#" class="done-button">Mark as done</a>
+				</li>		
+				<li>
+					<span class = "item done">learn pHp</span>
+				</li>	
+			</ul>
+			<form class="item-add" action="add.php"method="post">
+				<input type="text" name="name" placeholder="Type a new item to do here" class="input">
+				<input type="submit" value="Add" class="submit">
+			</form>
+	</body>
+</html>
