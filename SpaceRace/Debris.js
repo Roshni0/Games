1ff4880f17c0acb6c4a219c6428ba51159f3f59a
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-06-07 12:04:29 +0800

    Update Debris.js

--- SpaceRace/Debris.js
+++ SpaceRace/Debris.js
@@ -6,37 +6,37 @@ class Debris{
   resetDebris(){
     this.y=random(height-70);
     let spawnLeftSide=random(1)<0.5;
-    if (spawnLeftSide){
+    if(spawnLeftSide){
     	this.x=random(-width,0);	
-    	this.isGoingLeft = false;
-    } else {
-    	this.x = random(width, width * 2);
-      this.isGoingLeft = true;
+    	this.isGoingLeft=false;
+    }else{
+    	this.x=random(width,width*2);
+      this.isGoingLeft=true;
     }
   }
-  update() {
-  	if (this.isGoingLeft) {
+  update(){
+  	if(this.isGoingLeft){
     	this.x --;
-    } else {
+    }else{
     	this.x ++;
     }
-    if (this.isOffScreen()) {
+    if(this.isOffScreen()){
     	this.resetDebris();
     }
   }
-  isOffScreen() {
-    if (this.isGoingLeft && this.x < -5) {
+  isOffScreen(){
+    if(this.isGoingLeft && this.x<-5){
     	return true;
-    } else if(!this.isGoingLeft && this.x > width + 5) {
+    }else if(!this.isGoingLeft && this.x>width+5){
     	return true;
     }
     return false;
   }
-  display() {
-  	ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  display(){
+  	ellipse(this.x,this.y,this.r*2,this.r*2);
   }
-  hasHitShip(ship) {
-  	if (dist(this.x, this.y, ship.x, ship.y) < this.r + ship.r) {
+  hasHitShip(ship){
+  	if (dist(this.x,this.y,ship.x,ship.y)<this.r+ship.r){
     	return true;
     }
     return false
