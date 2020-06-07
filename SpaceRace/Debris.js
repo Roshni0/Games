e2eb0470186d169c354ab5bc9a84ea5c6a665bcf
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-06-07 12:02:26 +0800

    Update Debris.js

--- SpaceRace/Debris.js
+++ SpaceRace/Debris.js
@@ -1,13 +1,13 @@
-class Debris {
-  constructor() {
-    this.r = 5;
+class Debris{
+  constructor(){
+    this.r=5;
     this.resetDebris();
   }
-  resetDebris() {
-    this.y = random(height - 70);
-    let spawnLeftSide = random(1) < 0.5;
-    if (spawnLeftSide) {
-    	this.x = random(-width, 0);	
+  resetDebris(){
+    this.y=random(height-70);
+    let spawnLeftSide=random(1)<0.5;
+    if (spawnLeftSide){
+    	this.x=random(-width,0);	
     	this.isGoingLeft = false;
     } else {
     	this.x = random(width, width * 2);
