2d4529894aaf9f4c0df8180f075e9f2ac9d94edc
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-06-07 11:50:37 +0800

    Update Ship.js

--- SpaceRace/Ship.js
+++ SpaceRace/Ship.js
@@ -1,19 +1,49 @@
 class Ship {
  
-  constructor(x) {
+  /*constructor(x) {
     this.x = x;
     this.score = 0;
     this.respawn();
     this.r = 10;
-  }
+  }*/
  
   respawn() {
     this.y = height - 20;
     this.isUp = false
     this.isDown = false;
   }
-  
+ class Ship {
+ 
+  constructor(x, spaceshipImage) {
+    this.x = x;
+    this.score = 0;
+    this.r = 15;
+    this.respawn();
+    // store the image in the ship object
+    this.spaceshipImage = spaceshipImage;
+  }
+   
   update() {
+    if (this.isUp && this.y > 0) {
+        this.up();
+    } else if (this.isDown && this.y < height - 20) {
+        this.down();
+    }
+     
+    if (this.hasPlayerScoredAPoint()) {
+      this.score ++;
+      this.respawn();
+    }
+  }
+   
+  display() {
+     
+    // display our beautiful ship to the world!
+    imageMode(CENTER);
+    image(this.spaceshipImage, this.x, this.y, this.r*2, this.r*2);
+  }
+  
+ /* update() {
     if (this.isUp && this.y > 0) {
       this.up();
     } else if (this.isDown && this.y < height - 20) {
@@ -28,7 +58,7 @@ class Ship {
    
   display() {
     ellipse(this.x, this.y, this.r * 2, this.r * 2);
-  }
+  }*/
    
    
   up() {
