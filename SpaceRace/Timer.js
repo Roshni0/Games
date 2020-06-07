532b4cf6f18cae3b77ad9cea53fe96fbbea67bf0
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-06-07 12:22:37 +0800

    Update Timer.js

--- SpaceRace/Timer.js
+++ SpaceRace/Timer.js
@@ -1,10 +1,10 @@
-class Timer {
-  constructor() {
-  	this.x = width / 2;
-  	this.y = height / 2;
+class Timer{
+  constructor(){
+  	this.x=width/2;
+  	this.y=height/2;
   }
-  display() {
-  	rect(this.x, this.y, 5, height);
-  	this.y += 0.1;
+  display(){
+  	rect(this.x,this.y,5,height);
+  	this.y+=0.1;
   }
 }
