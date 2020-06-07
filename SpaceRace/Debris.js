d743f1f889fe715b29d7f38edcd5222898231aee
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-06-07 11:56:43 +0800

    Update Debris.js

--- SpaceRace/Debris.js
+++ SpaceRace/Debris.js
@@ -1,17 +1,11 @@
 class Debris {
-
-  
   constructor() {
     this.r = 5;
     this.resetDebris();
   }
-  
   resetDebris() {
-  	
     this.y = random(height - 70);
-    
     let spawnLeftSide = random(1) < 0.5;
-    
     if (spawnLeftSide) {
     	this.x = random(-width, 0);	
     	this.isGoingLeft = false;
@@ -19,23 +13,17 @@ class Debris {
     	this.x = random(width, width * 2);
       this.isGoingLeft = true;
     }
-    
-    
   }
-  
   update() {
   	if (this.isGoingLeft) {
     	this.x --;
     } else {
     	this.x ++;
     }
-    
     if (this.isOffScreen()) {
     	this.resetDebris();
     }
   }
-  
-  
   isOffScreen() {
     if (this.isGoingLeft && this.x < -5) {
     	return true;
@@ -44,16 +32,13 @@ class Debris {
     }
     return false;
   }
-  
   display() {
   	ellipse(this.x, this.y, this.r * 2, this.r * 2);
   }
-  
   hasHitShip(ship) {
   	if (dist(this.x, this.y, ship.x, ship.y) < this.r + ship.r) {
     	return true;
     }
     return false
   }
-
 }
