a2a4c8f9e77f18f16b95233d7fc5047676ecf894
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-06-13 12:28:54 +0800

    Update Jet.js

--- JetFighter/Jet.js
+++ JetFighter/Jet.js
@@ -18,54 +18,54 @@ class Jet{
     // new function to check if player has been hit
     this.processBeingHitByBullet(enemyPlayer);
   }
-  processBeingHitByBullet(enemyPlayer) {
-    let enemyBullets = enemyPlayer.bullets;
+  processBeingHitByBullet(enemyPlayer){
+    let enemyBullets=enemyPlayer.bullets;
     // I use 10 as the radius for the ship, you can tweak it
-    for (let i = enemyBullets.length - 1; i >= 0; i--) {
-	if (dist(this.x, this.y, enemyBullets[i].x, enemyBullets[i].y) < (10 + enemyBullets[i].r)){ 
-	enemyBullets.splice(i, 1);
+    for(let i=enemyBullets.length-1;i>=0;i--){
+	if (dist(this.x,this.y,enemyBullets[i].x,enemyBullets[i].y)<(10+enemyBullets[i].r)){ 
+	enemyBullets.splice(i,1);
 	enemyPlayer.score++;
       }
     }
   }
   goTheWayWereFacing(){
-    this.x += this.speed*cos(this.angle);
-    this.y += this.speed*sin(this.angle);
+    this.x+=this.speed*cos(this.angle);
+    this.y+=this.speed*sin(this.angle);
   }
   constrainToMap(){
-    if (this.x < -this.image.width) {
+    if(this.x<-this.image.width){
 	this.x=width;
-    } else if (this.x > width) {
+    }else if(this.x>width){
 	this.x=0;
     } 
-    if (this.y > height) {
+    if this.y>height){
 	this.y=0;
-    } else if (this.y < -this.image.height) {
+    }else if(this.y<-this.image.height){
 	this.y=height;
     }
   }
   // when the player shoots add to the bullet array
-shoot() {
+  shoot(){
     let bullet = new Bullet(this.x,this.y,this.angle,this.isWhite);
     this.bullets.push(bullet);
   }
-  draw() {
+  draw(){
     push();
-    translate(this.x, this.y);
+    translate(this.x,this.y);
     imageMode(CENTER);
-    rotate(this.angle + HALF_PI);
-    image(this.image, 0, 0);
+    rotate(this.angle+HALF_PI);
+    image(this.image,0,0);
     pop();
     // call to draw and update the buggers
     this.drawBullets();
   }
   // draw and update the buggers
-  drawBullets() {
-    for (let i = this.bullets.length - 1; i >= 0; i--) {
+  drawBullets(){
+    for(let i=this.bullets.length-1;i>=0;i--){
       this.bullets[i].update();
-	this.bullets[i].draw(); 
-      if (this.bullets[i].timeAlive > 200) {
-	this.bullets.splice(i, 1);
+      this.bullets[i].draw(); 
+      if(this.bullets[i].timeAlive>200){
+	this.bullets.splice(i,1);
       }
     }
   }
