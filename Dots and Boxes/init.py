605d66c64e1c281c634d0e542fff1b50a2a22773
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-06-21 22:01:54 +0800

    Update init.py

--- Dots and Boxes/init.py
+++ Dots and Boxes/init.py
@@ -98,65 +98,56 @@ class Dots_and_Boxes():
 #Drawing Functions:
     def make_edge(self,type,logical_position):
         if type=='row':
-            start_x = distance_between_dots/2 + logical_position[0]*distance_between_dots
-            end_x = start_x+distance_between_dots
-            start_y = distance_between_dots/2 + logical_position[1]*distance_between_dots
-            end_y = start_y
-        elif type == 'col':
-            start_y = distance_between_dots / 2 + logical_position[1] * distance_between_dots
-            end_y = start_y + distance_between_dots
-            start_x = distance_between_dots / 2 + logical_position[0] * distance_between_dots
-            end_x = start_x
+            start_x=distance_between_dots/2+logical_position[0]*distance_between_dots
+            end_x=start_x+distance_between_dots
+            start_y=distance_between_dots/2+logical_position[1]*distance_between_dots
+            end_y=start_y
+        elif type=='col':
+            start_y=distance_between_dots/2+logical_position[1]*distance_between_dots
+            end_y=start_y+distance_between_dots
+            start_x=distance_between_dots/2+logical_position[0]*distance_between_dots
+            end_x=start_x
         if self.player1_turn:
-            color = player1_color
+            color=player1_color
         else:
-            color = player2_color
-        self.canvas.create_line(start_x, start_y, end_x, end_y, fill=color, width=edge_width)
+            color=player2_color
+        self.canvas.create_line(start_x,start_y,end_x,end_y,fill=color,width=edge_width)
     def display_gameover(self):
-        player1_score = len(np.argwhere(self.board_status == -4))
-        player2_score = len(np.argwhere(self.board_status == 4))
-        if player1_score > player2_score:# Player 1 wins
-            text = 'Winner: Player 1 '
-            color = player1_color
-        elif player2_score > player1_score:
-            text = 'Winner: Player 2 '
-            color = player2_color
+        player1_score=len(np.argwhere(self.board_status==-4))
+        player2_score=len(np.argwhere(self.board_status==4))
+        if player1_score>player2_score:# Player 1 wins
+            text='Winner: Player 1 '
+            color=player1_color
+        elif player2_score>player1_score:
+            text='Winner: Player 2 '
+            color=player2_color
         else:
-            text = 'Its a tie'
-            color = 'gray'
+            text='Its a tie'
+            color='gray'
         self.canvas.delete("all")
-        self.canvas.create_text(size_of_board / 2, size_of_board / 3, font="cmr 60 bold", fill=color, text=text)
-        score_text = 'Scores \n'
-        self.canvas.create_text(size_of_board / 2, 5 * size_of_board / 8, font="cmr 40 bold", fill=Green_color,
-                                text=score_text)
-        score_text = 'Player 1 : ' + str(player1_score) + '\n'
-        score_text += 'Player 2 : ' + str(player2_score) + '\n'
-        self.canvas.create_text(size_of_board / 2, 3 * size_of_board / 4, font="cmr 30 bold", fill=Green_color,
-                                text=score_text)
-        self.reset_board = True
-        score_text = 'Click to play again \n'
-        self.canvas.create_text(size_of_board / 2, 15 * size_of_board / 16, font="cmr 20 bold", fill="gray",
-                                text=score_text)
+        self.canvas.create_text(size_of_board/2,size_of_board/3,font="cmr 60 bold",fill=color,text=text)
+        score_text='Scores \n'
+        self.canvas.create_text(size_of_board/2,5*size_of_board/8,font="cmr 40 bold",fill=Green_color,text=score_text)
+        score_text='Player 1 : '+str(player1_score)+'\n'
+        score_text+='Player 2 : '+str(player2_score)+'\n'
+        self.canvas.create_text(size_of_board/2,3*size_of_board/4,font="cmr 30 bold",fill=Green_color,text=score_text)
+        self.reset_board=True
+        score_text='Click to play again \n'
+        self.canvas.create_text(size_of_board/2,15*size_of_board/16,font="cmr 20 bold",fill="gray",text=score_text)
     def refresh_board(self):
         for i in range(number_of_dots):
             x = i*distance_between_dots+distance_between_dots/2
-            self.canvas.create_line(x, distance_between_dots/2, x,
-                                    size_of_board-distance_between_dots/2,
-                                    fill='gray', dash = (2, 2))
-            self.canvas.create_line(distance_between_dots/2, x,
-                                    size_of_board-distance_between_dots/2, x,
-                                    fill='gray', dash=(2, 2))
+            self.canvas.create_line(x,distance_between_dots/2,x,size_of_board-distance_between_dots/2,fill='gray',dash=(2,2))
+            self.canvas.create_line(distance_between_dots/2,x,size_of_board-distance_between_dots/2,x,fill='gray',dash=(2,2))
         for i in range(number_of_dots):
             for j in range(number_of_dots):
-                start_x = i*distance_between_dots+distance_between_dots/2
-                end_x = j*distance_between_dots+distance_between_dots/2
-                self.canvas.create_oval(start_x-dot_width/2, end_x-dot_width/2, start_x+dot_width/2,
-                                        end_x+dot_width/2, fill=dot_color,
-                                        outline=dot_color)
+                start_x=i*distance_between_dots+distance_between_dots/2
+                end_x=j*distance_between_dots+distance_between_dots/2
+                self.canvas.create_oval(start_x-dot_width/2,end_x-dot_width/2,start_x+dot_width/2,end_x+dot_width/2, fill=dot_color,outline=dot_color)
     def display_turn_text(self):
-        text = 'Next turn: '
+        text='Next turn: '
         if self.player1_turn:
-            text += 'Player1'
+            text+='Player1'
             color = player1_color
         else:
             text += 'Player2'
