f6e582e74d8937bbffc6589d79402e0bb5b9c88f
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-07-15 11:27:10 +0800

    Update flappybird.js

--- Flappy Bird/flappybird.js
+++ Flappy Bird/flappybird.js
@@ -1,77 +1,34 @@
 var cvs = document.getElementById("canvas");
-
 var ctx = cvs.getContext("2d");
-
-// load images
-
-var bird = new Image();
-
+var bird = new Image();//loading the images
 var bg = new Image();
-
 var fg = new Image();
-
 var pipeNorth = new Image();
-
 var pipeSouth = new Image();
-
 bird.src = "images/bird.png";
-
 bg.src = "images/bg.png";
-
 fg.src = "images/fg.png";
-
 pipeNorth.src = "images/pipeNorth.png";
-
 pipeSouth.src = "images/pipeSouth.png";
-
-
-
-// some variables
-
-var gap = 85;
-
+var gap = 85; // setting basic variables
 var constant;
-
 var bX = 10;
-
 var bY = 150;
-
 var gravity = 1.5;
-
 var score = 0;
-
-// audio files
-
-var fly = new Audio();
-
+var fly = new Audio(); //loading the audio
 var scor = new Audio();
-
 fly.src = "sounds/fly.mp3";
-
 scor.src = "sounds/score.mp3";
-
-// on key down
-
 document.addEventListener("keydown",moveUp);
-
 function moveUp(){
-
     bY -= 25;
-
     fly.play();
-
 }
-
-// pipe coordinates
-
 var pipe = [];
-
 pipe[0] = {
-
     x : cvs.width,
-
     y : 0
-
 };
 
 // draw images
